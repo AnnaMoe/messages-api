1aea3052728cdbceb62b42b23d9314de19f7e9ef
Anna Moelling <anna.moelling@googlemail.com>
2021-08-10 16:46:46 +0200

    implement ednpoint index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  # get this route /api/v1/restaurants
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :messages, only: [ :index ]
+      resources :messages, only: [ :index, :show ]
     end
   end
 end
